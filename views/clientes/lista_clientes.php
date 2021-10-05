57022aa2a5112fe50f0f5cd6c957b97a0c90439a
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-05 16:01:47 -0300

    Adicionando view lista_cliente

---
+++ views/clientes/lista_clientes.php
@@ -0,0 +1,46 @@
+<section>
+    <h3>
+        <i class="fa fa-users"></i>
+
+        <span>
+            Listar Clientes
+        </span>
+    </h3>
+
+</section>
+
+<section class="content">
+    <div class="box">
+        <div class="box-header with-border">
+            <div class="row margin-bottom-20">
+                <div class="col-md-12 text-right">
+                    <a href="create_cliente.php" title="Novo Cadastro" class="btn btn-success"><i
+                            class="fa fa-plus-circle"></i>
+                        Novo</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <table class="table table-dark table-hover table-sm">
+        <thead>
+            <tr>
+                <th>Nome </th>
+                <th class="text-center">R.G</th>
+                <th class="text-center">C.P.F</th>
+                <th class="text-center">Telefone</th>
+                <th class="text-right">Opções</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Nome</td>
+                <td class="text-center">R.G</td>
+                <td class="text-center">C.P.F</td>
+                <td class="text-center">Telefone</td>
+                <td class="text-right">Opções</td>
+
+            </tr>
+        </tbody>
+    </table>
+</section>
